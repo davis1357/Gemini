1fe291c96166b55549c0142f2b778e1ebe52ae9c
Lior <liorovedd1357@gmail.com>
2023-10-22 17:33:03 +0300

    Reverse Cube

--- Gemini/Assets/Player.cs
+++ Gemini/Assets/Player.cs
@@ -8,22 +8,25 @@ public class Player : MonoBehaviour
     [SerializeField] Camera mainCamera;
     [SerializeField] float playerSpeed = 10f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] bool isFlipped = false;
 
     Rigidbody2D myRigidBody;
     BoxCollider2D myCollider;
     bool isTouching = true;
+    float startingXPos;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myCollider = GetComponent<BoxCollider2D>();
+        startingXPos = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        mainCamera.transform.position = new Vector3(this.transform.position.x+11f, mainCamera.transform.position.y, -10);
+        mainCamera.transform.position = new Vector3(this.transform.position.x - startingXPos, mainCamera.transform.position.y, -10);
 
         PlayerMoves();
 
@@ -34,6 +37,7 @@ public class Player : MonoBehaviour
     {
         //myRigidBody.velocity = new Vector2(playerSpeed, 0);
         transform.Translate(Vector2.right * playerSpeed * Time.deltaTime);
+        //this.transform.position += this.transform.right * Time.deltaTime * playerSpeed;
     }
 
     private void PlayerJumping()
@@ -43,7 +47,10 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            myRigidBody.AddRelativeForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            if(!isFlipped)
+                myRigidBody.AddRelativeForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            else
+                myRigidBody.AddRelativeForce(Vector2.down * jumpSpeed, ForceMode2D.Impulse);
             //myRigidBody.AddRelativeForce(0,jumpSpeed,0);
             //myRigidBody.velocity = new Vector2(playerSpeed, jumpSpeed);
         }
